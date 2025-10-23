af113ad87ca905db0196f6c1529c6bd95f7695ff
Ehsan Golmakani <ehsan8184@gmail.com>
2025-10-23 23:51:53 +0330

    feat(firmware/scripts): Copy config on Prebuild

--- firmware/scripts/prebuild.py
+++ firmware/scripts/prebuild.py
@@ -23,5 +23,8 @@ def build_react_and_copy(source, target, env):
     if (DATA_DIR / "web").exists():
         shutil.rmtree(DATA_DIR / "web")
     shutil.copytree(DIST_DIR, DATA_DIR / "web")
+    
+    if not (DATA_DIR / "config.json").exists():
+        shutil.copyfile((PROJECT_DIR / "config.example.json"), (PROJECT_DIR / "data" / "config.json"))
 
 env.AddPreAction("upload", build_react_and_copy)
